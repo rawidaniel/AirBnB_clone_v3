151e57aa20206eef2981402298d4658d4af49860
rawidaniel <rawidaniel96@gmail.com>
2022-10-22 12:49:36 -0700

    reterive status of api

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+"""
+app for registering blueprint and starting flask
+"""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    '''close query after each session'''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
