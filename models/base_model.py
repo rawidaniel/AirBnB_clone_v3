de788239edf6f626482b57750275811dfb5e0ce7
rawidaniel <rawidaniel96@gmail.com>
2022-10-25 03:33:25 -0700

    secure hash

--- models/base_model.py
+++ models/base_model.py
@@ -58,16 +58,19 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
+    def to_dict(self, saving_file_storage=False):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
+        obj_class = self.__class__.__name__
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-        new_dict["__class__"] = self.__class__.__name__
+        new_dict["__class__"] = obj_class
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if not saving_file_storage and obj_class == 'User':
+            new_dict.pop('password', None)
         return new_dict
 
     def delete(self):
